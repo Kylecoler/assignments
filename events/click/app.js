a966f55e70a7196b437b4d4ab65dbccb28d90b4a
Kyle Allred <g.kyleallred@yahoo.com>
2019-02-01 11:21:47 -0700

    updates

--- events/click/app.js
+++ events/click/app.js
@@ -1,1 +1,5 @@
 // make the box disapear when the user clicks it
+
+document.getElementById("red-box").addEventListener("click", function(e){
+    e.style.color = "white"
+})
---
+++ projects/MarioPestControl/app.js
@@ -0,0 +1,13 @@
+document.getElementById("goomba").addEventListener("change"){
+    document.getElementById("ggone").innerHTML += "goomba" * 5
+} 
+
+document.getElementById("bob-omb").addEventListener("change"){
+    document.getElementById("bgone").innerHTML += "bob-omb" *7
+}
+
+document.getElementById("cheep").addEventListener("change"){
+    document.getElementById("cgone").innerHTML += "cheep" * 10
+}
+
+"ggone" + "bgone" + "cgone" = "total"
