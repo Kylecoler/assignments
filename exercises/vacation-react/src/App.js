acd838ed11653cf10cb7ba07ac9d52fd1ada2f16
Kyle Allred <g.kyleallred@yahoo.com>
2019-02-21 14:03:31 -0700

    updates

--- exercises/vacation-react/src/App.js
+++ exercises/vacation-react/src/App.js
@@ -1,38 +1,58 @@
 import React from 'react';
+import Arr from "./arr"
 
 const App = () => {
-    return (
-        <div>
+    const vacspot = Arr.map(spot => {
+            
 
+        const style1 = {
+            height: "210px"
+        }
+        if(spot.timeToGo === "Spring"){
+            style1.background = "linear-gradient(yellow, skyblue 80%, green 10%)"
+        } else if(spot.timeToGo === "Summer"){
+            style1.background = "linear-gradient(yellow,orange,red)"
+        } else if (spot.timeToGo === "Winter"){
+            style1.background = "linear-gradient(white, aqua, blue)"
+        } else {
+            style1.background = "linear-gradient(gold 50%, brown, darkred)"
+
+        }
+        
+        let cosst = ()=>{
+            if(spot.price <= 500){
+                return "$"
+            } else if (spot.price > 500 && spot.price < 1000){
+                return "$$"
+            } else {
+                return "$$$"
+            }
+        }
+
+
+        return (
+            <div style={style1}>
+                <h1>{spot.place}</h1>
+                <h3>Best time to see: {spot.timeToGo}</h3>
+                <p>Cost: {cosst()}{spot.price}</p>
+            </div>
+        )
+    })
+
+
+    const style = {
+        display:"grid",
+        gridTemplateColumns: "repeat(auto-fit,350px)",
+        gridGap:"5px",
+        textAlign: "center",
+        justifyContent: "space-around"
+    }
+
+    return (
+        <div style={style}>
+            {vacspot}
         </div>
     )
 }
 
 export default App
-
-let vacationSpots = [
-    {
-      place: "Meridian, Idaho",
-      price: 40,
-      timeToGo: "Spring"
-    },{
-      place: "Cancun",
-      price: 900,
-      timeToGo: "Winter"
-    },{
-      place: "China",
-      price: 1200,
-      timeToGo: "Fall"
-    },{
-      place: "Russia",
-      price: 1100,
-      timeToGo: "Summer"
-    },{
-      place: "Lebanon",
-      price: 400,
-      timeToGo: "Spring"
-    }
-  ]
-
-// Change the background color of your card depending on the timeToGo property.
-// Give each card 1, 2, or 3 dollar signs ($) depending on if it's less than $500, less than $1000, or more than $1000
