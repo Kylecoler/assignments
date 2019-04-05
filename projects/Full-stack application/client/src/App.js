b67b3d5411ff9c62303473d62cc0f85e05d39e16
Kyle Allred <g.kyleallred@yahoo.com>
2019-04-05 14:14:06 -0600

    working

--- projects/Full-stack application/client/src/App.js
+++ projects/Full-stack application/client/src/App.js
@@ -1,54 +1,10 @@
 import React from 'react'
+import Calender from './calendar'
 
 const App = ()=>{
-    const today = new Date()
-    // const year = today.getFullYear()
-    // const month = today.getMonth()
-    // const monthList=['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    const startDate = today.getDate()
-    //step one make rows
-    //step two fill rows with cells
-    const rows = []
-    const body = ()=>{
-        let date = 1
-        for(let i = 0;i<6;i++){
-            const dates = []
-            for(let j=0;j<7;j++){
-                dates.push(<td>{date}</td>)
-                date++
-                console.log(rows)
-            }
-            rows.push(<tr>{
-                dates.map(day=>{
-                    return(day)
-                })
-            }</tr>)
-
-        }
-    }
-
-    body()
     return(
         <div>
-            {startDate}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Sun</th>
-                        <th>Mon</th>
-                        <th>Tue</th>
-                        <th>Wed</th>
-                        <th>Thur</th>
-                        <th>Fri</th>
-                        <th>Sat</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows.map(row=>{
-                        return row
-                    })}
-                </tbody>
-            </table>
+            <Calender/>
         </div>
     )
 }
