e8637fd97ee111615d33dd93deed28958ef648dc
Kyle Allred <g.kyleallred@yahoo.com>
2019-04-08 08:58:45 -0600

    hm

--- exercises/forception/app.js
+++ exercises/forception/app.js
@@ -1,6 +1,6 @@
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
 var alphabet = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var newarr = []
+
 
 function arr(one, two){
     for(i=0; i<one.length; i++){
