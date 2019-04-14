f3de432c6fa28479321eb4e53392b90150654c0a
Kylecoler <g.kyleallred@yahoo.com>
2019-04-13 21:37:12 -0600

    refining

--- events/click/app.js
+++
@@ -1,5 +0,0 @@
-// make the box disapear when the user clicks it
-
-document.getElementById("red-box").addEventListener("click", function(e){
-    e.style.color = "white"
-})
--- events/submit/app.js
+++
@@ -1,3 +0,0 @@
-// The "submit" event refreshed your brower by default
-// use "e.preventDefault" in the first line of your callback function to prevent this
-// remember to pass "e" into the callback function
