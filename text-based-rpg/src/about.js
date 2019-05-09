3da0639c259dcb777ec5a76388d32ddf4e50141c
Kylecoler <g.kyleallred@yahoo.com>
2019-05-09 12:16:25 -0600

    saving

--- text-based-rpg/src/about.js
+++ text-based-rpg/src/about.js
@@ -3,7 +3,12 @@ import React from 'react'
 const About = ()=>{
     return(
         <div>
-            <img src="handsome.jpg" alt="" srcset=""/>
+            <img src={require("./images/handsome.jpg")} alt="My Face" height="300pt"/>
+            <p>Many years ago i discoverd "text based adventures", and since then i have always want'd to play them.
+                now that I am a software engineer i figured i might as well build my own.
+            </p>
+            <p>linkdin</p>
+            <p>github</p>
         </div>
     )
 }
