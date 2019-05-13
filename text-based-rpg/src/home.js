0713c2facc6b400fd426c940f83e5551c84f93ac
Kylecoler <g.kyleallred@yahoo.com>
2019-05-13 12:27:45 -0600

    simple fix

--- text-based-rpg/src/home.js
+++ text-based-rpg/src/home.js
@@ -1,26 +1,19 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 
-class Home extends Component{
-    constructor(){
-        super()
-        this.state={
 
-        }
-    }
-    render(){
-        return(
-            <div>
-                <div className='gameCard'>
-                    <Link to='game1' className='gameLink'>
-                        <h3>Game Name</h3>
-                        <img src={require("./images/rpg.jpg")} alt="Game pic" width='250pt' height='250pt'/>
-                        <p>Summary</p>
-                    </Link>
-                </div>
+const Home=()=>{
+    return(
+        <div>
+            <div className='gameCard'>
+                <Link to='game1' className='gameLink'>
+                    <h3>Game Name</h3>
+                    <img src={require("./images/rpg.jpg")} alt="Game pic" width='250pt' height='250pt'/>
+                    <p>Summary</p>
+                </Link>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Home
