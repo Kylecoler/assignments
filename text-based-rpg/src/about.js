65df8c70cdd767f54adf3dd67476913bbdfdc71d
Kylecoler <g.kyleallred@yahoo.com>
2019-05-15 11:32:37 -0600

    updates

--- text-based-rpg/src/about.js
+++ text-based-rpg/src/about.js
@@ -4,8 +4,8 @@ const About = ()=>{
     return(
         <div>
             <img src={require("./images/handsome.jpg")} alt="My Face" height="300pt"/>
-            <p>Many years ago i discoverd "text based adventures", and since then i have always want'd to play them.
-                now that I am a software engineer i figured i might as well build my own.
+            <p>Many years ago I discoverd "text based adventures", and since then I have always wanted to play them.
+                now that I am a software engineer I figured I might as well build my own.
             </p>
             <p>linkdin</p>
             <p>github</p>
--- text-based-rpg/src/game1.js
+++ text-based-rpg/src/game1.js
@@ -4,14 +4,51 @@ class Game1 extends Component{
     constructor(){
         super()
         this.state={
-
+            text:"Welcome Adventurer, what shall we call you by?",
+            userInput:'',
+            name:'',
         }
+    }
+
+    readfunction=(e)=>{
+        e.preventDefault()
+        if(!this.state.name){
+            this.setState(()=>{
+                return{name:this.state.userInput,
+                       text:`Greetings ${this.state.userInput}, our village has been raided by undead will you help us?  Yes or No`,
+                       userInput:''
+                }
+            })
+        } else {
+            switch(this.state.userInput){
+                case 'left':
+                    this.setState(()=>{
+                        return{text:'you have turned left'}
+                    })
+                    break;
+                default:
+                    this.setState(()=>{
+                        return{text:"Either you spelled something wrong or you found a path I didn't plan on"}
+                    })
+            }
+        }
+    }
+
+    change=(e)=>{
+        const value = e.target.value
+        this.setState(()=>{
+            return{userInput:value}
+        })
     }
 
     render(){
         return(
-            <div>
-                Game1
+            <div className='gameContainer'>
+                <p>{this.state.text}</p>
+                <form onSubmit={this.readfunction}>
+                    <input type="text" onChange={this.change} value={this.state.userInput} className='input' name='input'/>
+                    <input type="submit" className='submit'/>
+                </form>
             </div>
         )
     }
