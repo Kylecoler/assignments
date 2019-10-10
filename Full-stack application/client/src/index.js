877fc2b69277606922156cee0f2e4a3bac97841d
Kylecoler <g.kyleallred@yahoo.com>
2019-10-10 11:52:12 -0600

    minor updates

--- Full-stack application/client/src/index.js
+++ Full-stack application/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom'
-import App from './app'
+import App from './App'
 ReactDOM.render(
     <BrowserRouter>
         <App/>
