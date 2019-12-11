c8747533d82f62b6fe8acef0ab72f880d6dc3d56
Kylecoler <g.kyleallred@yahoo.com>
2019-12-11 03:36:10 -0700

    added dice functionality

--- Random/index.js
+++ Random/index.js
@@ -1,3 +1,10 @@
 const rndnum = function(){
-    document.getElementById('result').innerHTML = Math.floor(Math.random() * 10)
+    let DieVal = document.getElementById("DiceValue").value
+    let DieNum = document.getElementById("DiceNum").value
+    const Result = []
+    for (i = 0; i < DieNum; i++){
+        Result.push(Math.floor(Math.random() * Math.floor(DieVal))+1)
+    }
+    // document.getElementById('result').innerHTML = Math.floor(Math.random() * 10)
+    document.getElementById('result').innerHTML = Result
 }
