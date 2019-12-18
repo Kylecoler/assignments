27271265e358ad396eb24850f17d524582c58a3d
Kylecoler <g.kyleallred@yahoo.com>
2019-12-18 09:49:26 -0700

    adding animations

--- Random/index.js
+++ Random/index.js
@@ -1,10 +1,13 @@
 const rndnum = function(){
+    document.getElementById('animation').classList.add('go')
+        reset=()=>{
+            document.getElementById('animation').classList.remove('go')
+        }
     let DieVal = document.getElementById("DiceValue").value
     let DieNum = document.getElementById("DiceNum").value
     const Result = []
     for (i = 0; i < DieNum; i++){
         Result.push(Math.floor(Math.random() * Math.floor(DieVal))+1)
     }
-    // document.getElementById('result').innerHTML = Math.floor(Math.random() * 10)
     document.getElementById('result').innerHTML = Result
 }
