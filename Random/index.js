9ab2a1752a5f7c68d35334513b00e3a3ece5c7de
Kylecoler <g.kyleallred@yahoo.com>
2019-12-26 12:20:29 -0700

    dice roll per size

--- Random/index.js
+++ Random/index.js
@@ -1,17 +1,29 @@
+
 const rndnum = function(){
-    document.getElementById('animation').classList.add('go')
-    document.getElementById('d4animation').classList.add('go')
-    setTimeout(reset, 5000)
     let DieVal = document.getElementById("DiceValue").value
     let DieNum = document.getElementById("DiceNum").value
+    document.getElementById('d6animation').classList.add('go')
+    document.getElementById('d4animation').classList.add('go')
+    setTimeout(reset, 5000)
     const Result = []
     for (i = 0; i < DieNum; i++){
         Result.push(Math.floor(Math.random() * Math.floor(DieVal))+1)
     }
     document.getElementById('result').innerHTML = Result
+    if(DieVal == 4){
+        document.getElementById("d4animation").classList.toggle('hide')
+        setTimeout(toggle=()=>{
+            document.getElementById('d4animation').classList.toggle('hide')
+        },5000)
+    } else if(DieVal == 6){
+        document.getElementById("d6animation").classList.toggle('hide')
+        setTimeout(toggle=()=>{
+            document.getElementById('d6animation').classList.toggle('hide')
+        },5000)
+    }
 }
 
 reset = function(){
-    document.getElementById('animation').classList.remove('go')
+    document.getElementById('d6animation').classList.remove('go')
     document.getElementById('d4animation').classList.remove('go')
 }
