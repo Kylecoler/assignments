a8c0d9d3dd309e6a8af6a5aac0c4de71bdfa730e
Kylecoler <g.kyleallred@yahoo.com>
2020-01-05 05:43:43 -0700

    more stuff

--- Random/index.js
+++ Random/index.js
@@ -26,6 +26,24 @@ const rndnum = function(){
         setTimeout(toggle=()=>{
             document.getElementById('d8animation').classList.toggle('hide')
         },5000)
+    } else if(DieVal == 10){
+        document.getElementById('d10animation').classList.add('go')
+        document.getElementById('d10animation').classList.toggle('hide')
+        setTimeout(toggle=()=>{
+            document.getElementById('d10animation').classList.toggle('hide')
+        },5000)
+    } else if(DieVal == 12){
+        document.getElementById('d12animation').classList.add('go')
+        document.getElementById('d12animation').classList.toggle('hide')
+        setTimeout(toggle=()=>{
+            document.getElementById('d12animation').classList.toggle('hide')
+        },5000)
+    } else if(DieVal == 20){
+        document.getElementById('d20animation').classList.add('go')
+        document.getElementById('d20animation').classList.toggle('hide')
+        setTimeout(toggle=()=>{
+            document.getElementById('d20animation').classList.toggle('hide')
+        },5000)
     }
 }
 
@@ -33,6 +51,9 @@ reset = function(){
     document.getElementById('d4animation').classList.remove('go')
     document.getElementById('d6animation').classList.remove('go')
     document.getElementById('d8animation').classList.remove('go')
+    document.getElementById('d10animation').classList.remove('go')
+    document.getElementById('d12animation').classList.remove('go')
+    document.getElementById('d20animation').classList.remove('go')
 }
 // possibly adding cloned elements
 // add cool D&D background
