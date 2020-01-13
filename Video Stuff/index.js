7bb929820bd2dba65412e31409ef7e9d63870127
Kylecoler <g.kyleallred@yahoo.com>
2020-01-13 14:25:17 -0700

    canvas fun

--- Video Stuff/index.js
+++ Video Stuff/index.js
@@ -1,19 +1,19 @@
 const canvas = document.getElementById('canvas')
-let ctx = canvas.getContext("2d")
+// let ctx = canvas.getContext("2d")
 
 //canvas color
-ctx.fillStyle = "#FF6CFF"
-ctx.fillRect(0, 0, 125, 200)
+// ctx.fillStyle = "#FF6CFF"
+// ctx.fillRect(0, 0, 150, 200)
 
 //line
-ctx.moveTo(0,0)
-ctx.lineTo(250, 200)
-ctx.stroke()
+// ctx.moveTo(0,0)
+// ctx.lineTo(250, 200)
+// ctx.stroke()
 
 //circle
-ctx.beginPath()
-ctx.arc(95, 50, 40, 0, 2 * Math.PI)
-ctx.stroke()
+// ctx.beginPath()
+// ctx.arc(95, 50, 40, 0, 2 * Math.PI)
+// ctx.stroke()
 
 //create gradient
 // let grd = ctx.createLinearGradient(0, 0, 200, 0)
@@ -24,3 +24,10 @@ ctx.stroke()
 //fiil with gradient
 // ctx.fillStyle = grd
 // ctx.fillRect(0, 0, 250, 200)
+
+let rect = document.querySelector('canvas').getboundingClientRect()
+function cursorPosition(event){
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    console.log(x, y)
+}
