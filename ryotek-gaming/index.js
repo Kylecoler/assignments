6aed0f2ab9764f814bc5ea05683d1f9d2970f1e1
Kylecoler <g.kyleallred@yahoo.com>
2021-01-09 18:05:52 -0700

    minor addons

--- ryotek-gaming/index.js
+++ ryotek-gaming/index.js
@@ -1,3 +1,16 @@
 let display = function(e){
     document.getElementById(e).classList.toggle("show")
 }
+
+// window.onclick = function(event){
+//     if(event.target.matches('.dropbtn')){
+//         let dropdowns = document.getElementsByClassName("dropdown-content");
+//         for (i = 0, i < dropdowns.length; i++;){
+//             let openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')){
+//                 openDropdown.classList.remove('show')
+//             }
+//         }
+//     }
+// }
+
