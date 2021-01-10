2a748131363e95c51c667f4249a222c304d12603
Kylecoler <g.kyleallred@yahoo.com>
2021-01-10 04:05:57 -0700

    more building

--- Ryotek-gaming react/ryoteck-react/src/index.js
+++ Ryotek-gaming react/ryoteck-react/src/index.js
@@ -1,17 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Nav from './nav'
 
-function Nav(){
-    return(
-        <nav>
-            <ul>
-                <li>Ryoteck Corporation</li>
-                <li>Ryoteck Restorations</li>
-                <li>Fatsquacth Gaming</li>
-                <li>Gregador the Only</li>
-            </ul>
-        </nav>
-    )
-}
 
 ReactDOM.render(<Nav />,document.getElementById('root'))
---
+++ Ryotek-gaming react/ryoteck-react/src/nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav(){
+    return(
+        <nav>
+            <ol>
+                <li>Ryoteck Corporation</li>
+                <li>Ryoteck Restorations</li>
+                <li>Fatsquacth Gaming</li>
+                <li>Gregador the Only</li>
+            </ol>
+        </nav>
+    )
+}
+
+export default Nav
