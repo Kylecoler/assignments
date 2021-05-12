27e5ef601f4e2f4b111a04b37710a50370bb7495
Kylecoler <g.kyleallred@yahoo.com>
2021-05-12 05:50:24 -0600

    challenges

--- Edibit challenges/challenge.js
+++ Edibit challenges/challenge.js
@@ -2,11 +2,10 @@
 
 function findPerimeter(height, width) {
 	return (height*2)+(width*2)
+	// Test.assertEquals(findPerimeter(6, 7), 26)
+	// Test.assertEquals(findPerimeter(20, 10), 60)
+	// Test.assertEquals(findPerimeter(2, 9), 22)
 }
-
-Test.assertEquals(findPerimeter(6, 7), 26)
-Test.assertEquals(findPerimeter(20, 10), 60)
-Test.assertEquals(findPerimeter(2, 9), 22)
 
 //check if an integer is divisible by five
 
@@ -16,15 +15,15 @@ function divisibleByFive(n) {
 	} else {
 		return false
 	}
+	// Test.assertEquals(divisibleByFive(7), false)
+	// Test.assertEquals(divisibleByFive(5), true)
+	// Test.assertEquals(divisibleByFive(15), true)
+	// Test.assertEquals(divisibleByFive(33), false)
+	// Test.assertEquals(divisibleByFive(-18), false)
+	// Test.assertEquals(divisibleByFive(999), false)
+	// Test.assertEquals(divisibleByFive(2), false)
 }
 
-Test.assertEquals(divisibleByFive(7), false)
-Test.assertEquals(divisibleByFive(5), true)
-Test.assertEquals(divisibleByFive(15), true)
-Test.assertEquals(divisibleByFive(33), false)
-Test.assertEquals(divisibleByFive(-18), false)
-Test.assertEquals(divisibleByFive(999), false)
-Test.assertEquals(divisibleByFive(2), false)
 
 //profitable gamble
 
@@ -34,30 +33,30 @@ function profitableGamble(prob, prize, pay) {
 	} else {
 		return false
 	}
+	// Test.assertEquals(profitableGamble(0.2, 50, 9), true)
+	// Test.assertEquals(profitableGamble(0.9, 1, 2), false)
+	// Test.assertEquals(profitableGamble(0.9, 3, 2), true)
+	// Test.assertEquals(profitableGamble(0.33, 10, 3.30), true)
+	// Test.assertEquals(profitableGamble(0, 1000, 0.01), false)
+	// Test.assertEquals(profitableGamble(0.1, 1000, 7), true)
+	// Test.assertEquals(profitableGamble(0, 0, 0), false)
 }
 
-Test.assertEquals(profitableGamble(0.2, 50, 9), true)
-Test.assertEquals(profitableGamble(0.9, 1, 2), false)
-Test.assertEquals(profitableGamble(0.9, 3, 2), true)
-Test.assertEquals(profitableGamble(0.33, 10, 3.30), true)
-Test.assertEquals(profitableGamble(0, 1000, 0.01), false)
-Test.assertEquals(profitableGamble(0.1, 1000, 7), true)
-Test.assertEquals(profitableGamble(0, 0, 0), false)
 
 // Convert a Number to Base 2
 
 function binary(decimal) {
 	return decimal.toString(2)
+	// Test.assertEquals(binary(100), "1100100")
+	// Test.assertEquals(binary(1), "1")
+	// Test.assertEquals(binary(0), "0")
+	// Test.assertEquals(binary(69), "1000101")
+	// Test.assertEquals(binary(1023), "1111111111")
+	// Test.assertEquals(binary(511), "111111111")
+	// Test.assertEquals(binary(666), "1010011010")
+	// Test.assertEquals(binary(123), "1111011")
 }
 
-Test.assertEquals(binary(100), "1100100")
-Test.assertEquals(binary(1), "1")
-Test.assertEquals(binary(0), "0")
-Test.assertEquals(binary(69), "1000101")
-Test.assertEquals(binary(1023), "1111111111")
-Test.assertEquals(binary(511), "111111111")
-Test.assertEquals(binary(666), "1010011010")
-Test.assertEquals(binary(123), "1111011")
 
 //Add up the Numbers from a Single Number
 
